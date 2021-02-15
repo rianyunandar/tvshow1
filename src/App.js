941b3165e804aee83e7b8338f0bc923baa548146
rianyunandar <rianyunandar@gmail.com>
2021-02-15 16:45:25 +0700

    delete unused component & add react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, welcome to
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img alt="" src="/logo.png" width="45" height="30" className="d-inline-block align-top" />
+       </header>
     </div>
   );
 }
