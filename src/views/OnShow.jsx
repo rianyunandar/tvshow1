c881a71c816aafc27f2d6403613e649398d913cc
rianyunandar <rianyunandar@gmail.com>
2021-02-17 15:18:54 +0700

    update onshow

--- src/views/OnShow.jsx
+++ src/views/OnShow.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Row, Col, Container, Card } from "react-bootstrap";
+import { Row, Col, Container, Card,Media,Image } from "react-bootstrap";
 import moment from "moment";
+import ReactPaginate from 'react-paginate';
 
 const idLocale = require("moment/locale/id");
 
@@ -11,27 +12,53 @@ export class OnShow extends Component {
     this.state = {
       onShow: [],
       onShow2: [],
-      loading: true
+      loading: true,
+      perPage: 4,
+      currentPage: 0,
+      offset: 0
     };
+    this.handlePageClick = this
+    .handlePageClick
+    .bind(this);
   }
 
-  async componentDidMount() {
+ 
+  async receivedData() {
     let today = new Date().toISOString().slice(0, 10);
     try {
       const response = await fetch(
         `http://api.tvmaze.com/schedule/web?date=${today}&country=US`
       );
       const response2 = await fetch(`https://api.tvmaze.com/schedule`);
-      const json = await response.json();
-      const json2 = await response2.json();
+      const json = await response.json()
+      const json2 = await response2.json()
+      const slicejson = json.slice(this.state.offset, this.state.offset + this.state.perPage)
+      const slicejson2 = json2.slice(this.state.offset, this.state.offset + this.state.perPage)
+
       this.setState({
-        onShow: json,
-        onShow2: json2,
+        onShow: slicejson,
+        onShow2: slicejson2,
         loading: false
       });
     } catch (error) {
       alert(JSON.stringify(error.message));
     }
+  }
+  
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState({
+        currentPage: selectedPage,
+        offset: offset
+    }, () => {
+        this.receivedData()
+    });
+
+};
+  async componentDidMount() {
+    this.receivedData();
   }
   render() {
     return (
@@ -40,23 +67,37 @@ export class OnShow extends Component {
         {this.state.loading ? (
           <div>Loading...</div>
         ) : (
-          <div>
-            <h3 className="headerTitle">TV Show Today </h3>  
-            <Row>
-              {this.state.onShow2.map((item, i) => {
+                <div>
+                  <h3 className="headerTitle">TV Show Today </h3> 
+                  <ul className="list-unstyled border">
+                  {this.state.onShow2.map((item, i) => {
                 return (
-                  <Col key={i} lg={3} md={4} sm={6} xs={12}>
-                    <Card className='my-3 p-3 rounded'>
-                    <Card.Img className='todayCard' 
-                    src={item.show.image ? item.show.image.medium :"/logo.png"}
-                    variant='top' />
-                    <div>{item.show.name}</div>
-                    <div>{item.show.schedule.time}</div>
-                    </Card>
-                  </Col>
-                );
-              })}
-            </Row>
+        <Media as="li" key={i}>
+         
+           <Image  className='todayCard' src={item.show.image ? item.show.image.medium :"/logo.png"}  alt="Generic placeholder" rounded/>
+          <Media.Body>
+            <h5>{item.show.name} <span>  </span>{item.show.schedule.time}</h5>
+            <div>
+            <div dangerouslySetInnerHTML={ { __html: item.show.summary } }></div>
+            </div>           
+          </Media.Body>
+        </Media>
+           );
+          })}
+        </ul> 
+        <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+
             <h3 className="headerTitle">Web/Streaming Today</h3>
             <Row>
               {this.state.onShow.map((show,id) => (
