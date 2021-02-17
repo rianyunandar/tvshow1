d2938f9ffbb839f42343ef283b8a7e9d84d131e5
rianyunandar <rianyunandar@gmail.com>
2021-02-17 15:54:28 +0700

    update show

--- src/views/OnShow.jsx
+++ src/views/OnShow.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Container, Card,Media,Image } from "react-bootstrap";
+import { Row, Col, Container,Media,Image } from "react-bootstrap";
 import moment from "moment";
 import ReactPaginate from 'react-paginate';
 
@@ -10,7 +10,6 @@ export class OnShow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      onShow: [],
       onShow2: [],
       loading: true,
       perPage: 4,
@@ -24,19 +23,13 @@ export class OnShow extends Component {
 
  
   async receivedData() {
-    let today = new Date().toISOString().slice(0, 10);
     try {
-      const response = await fetch(
-        `http://api.tvmaze.com/schedule/web?date=${today}&country=US`
-      );
+     
       const response2 = await fetch(`https://api.tvmaze.com/schedule`);
-      const json = await response.json()
       const json2 = await response2.json()
-      const slicejson = json.slice(this.state.offset, this.state.offset + this.state.perPage)
       const slicejson2 = json2.slice(this.state.offset, this.state.offset + this.state.perPage)
 
       this.setState({
-        onShow: slicejson,
         onShow2: slicejson2,
         loading: false
       });
@@ -63,28 +56,29 @@ export class OnShow extends Component {
   render() {
     return (
       <Container>
-        {(console.log(this.state.onShow2), console.log(moment.locale()))}
         {this.state.loading ? (
           <div>Loading...</div>
         ) : (
                 <div>
                   <h3 className="headerTitle">TV Show Today </h3> 
+                  <Row>
                   <ul className="list-unstyled border">
+                    
                   {this.state.onShow2.map((item, i) => {
                 return (
-        <Media as="li" key={i}>
-         
+        <Media as="li"key={i} >
            <Image  className='todayCard' src={item.show.image ? item.show.image.medium :"/logo.png"}  alt="Generic placeholder" rounded/>
           <Media.Body>
-            <h5>{item.show.name} <span>  </span>{item.show.schedule.time}</h5>
+            <h5>{item.show.type} <span>  <i class="far fa-clock"></i> </span>{item.show.schedule.time} <span> On </span>{item.show.network.name} Channel</h5>
             <div>
-            <div dangerouslySetInnerHTML={ { __html: item.show.summary } }></div>
+            <div className='todaySummary' dangerouslySetInnerHTML={ { __html: item.show.summary } }></div>
             </div>           
           </Media.Body>
         </Media>
            );
           })}
         </ul> 
+        </Row>
         <ReactPaginate
                     previousLabel={"prev"}
                     nextLabel={"next"}
@@ -98,26 +92,7 @@ export class OnShow extends Component {
                     subContainerClassName={"pages pagination"}
                     activeClassName={"active"}/>
 
-            <h3 className="headerTitle">Web/Streaming Today</h3>
-            <Row>
-              {this.state.onShow.map((show,id) => (
-                <Col key={id} lg={3} md={4} sm={6} xs={12}>
-                <Card className='my-3 p-3 rounded'>
-                  <Card.Img className='todayCard' 
-                  src={show._embedded.show.image ? show._embedded.show.image.medium : "/logo.png"}
-                  variant='top' />
-                    <Card.Body>
-                      <Card.Title as='h5'>{show._embedded.show.name}</Card.Title>
-                      <Card.Text as='h6'>Eps : {show.name}</Card.Text>
-                      <Card.Text as='h6'>
-                        {moment(show.airstamp).format("DD/MM/YYYY, hh:mm")}
-                      </Card.Text>
-                    </Card.Body>
-                </Card>
-              </Col>
-              ))}
-            </Row>
-          </div>
+         </div>
         )}
       </Container>
     );
